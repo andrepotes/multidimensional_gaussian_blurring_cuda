1261566d14c610af9377d7d9a7d824e96653944a
andrepotes <andre.potes@gmail.com>
2025-10-04 14:34:13 +0400

    Use HOME-based paths instead of hardcoded absolute paths

--- gaussian_blurring.h
+++ gaussian_blurring.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#define IMAGE_FILE_PATH "/home/andrepotes/projects/learning_concurrent_programming/gaussian_blurring_cuda/test_image_1920_1080.jpg"
-#define OUTPUT_IMAGE_FILE_PATH "/home/andrepotes/projects/learning_concurrent_programming/gaussian_blurring_cuda/blurred_image.jpg"
+// Paths derived from the user's HOME directory to avoid hardcoded usernames
+#include <cstdlib>
+#include <string>
 
 #define NUM_OF_CHANNELS 1
 
@@ -9,6 +10,23 @@
 #include <memory>
 
 #include <opencv2/opencv.hpp>
+
+inline std::string getHomeDirectory() {
+    const char *home_env = std::getenv("HOME");
+    if (home_env != nullptr && home_env[0] != '\0') {
+        return std::string(home_env);
+    }
+    std::cerr << "Error: HOME environment variable is not set." << std::endl;
+    return std::string();
+}
+
+inline std::string getImageFilePath() {
+    return getHomeDirectory() + "/projects/learning_concurrent_programming/gaussian_blurring_cuda/test_image_1920_1080.jpg";
+}
+
+inline std::string getOutputImageFilePath() {
+    return getHomeDirectory() + "/projects/learning_concurrent_programming/gaussian_blurring_cuda/blurred_image.jpg";
+}
 
 struct ImageData {
     // Unique pointer for the float array, using a custom deleter for arrays
